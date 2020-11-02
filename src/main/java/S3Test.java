567487de9337b1178fe737b3509e96f2ee56f814
Stephen Cuppett <scuppett@redhat.com>
2020-11-02 12:09:51 -0500

    Adding initial sample files

---
+++ src/main/java/S3Test.java
@@ -0,0 +1,19 @@
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
+public class S3Test {
+
+    public static void main(String[] args) {
+
+        //Region region = Region.US_WEST_2;
+        S3Client s3 = S3Client.builder().build();
+
+        // List buckets
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+        listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
+
+    }
+
+}
